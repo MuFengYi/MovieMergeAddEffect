100ac396879e8b500d7c4610d03035e6e50854d6
CrazyYbf <macromacro@macrodeimac.local>
2016-07-01 18:19:59 +0800

    增加几个转场效果  啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊

--- MovieMerageAddEffect/MovieMerageAddEffect/AppDelegate.m
+++ MovieMerageAddEffect/MovieMerageAddEffect/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryPlayback error:nil];
     return YES;
 }
 
